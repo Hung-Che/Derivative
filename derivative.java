dd4e893c32f09880b3d008f0922142cd099815fb
Hung Che <55414511+Hung-Che@users.noreply.github.com>
2019-09-18 16:30:45 -0500

    Update derivative.java

--- derivative.java
+++ derivative.java
@@ -1,1 +1,25 @@
-
+public class Derivative{
+    public Derivative(String t){
+        t = "";
+    }
+    public static String getDev(String t){
+        int l = t.length();
+        String coe = "";
+        for(int i = 0; i < l; i++){
+            if(!t.substring(i, i+1).equals("x")||
+            !t.substring(i, i+1).equals("*")){
+                coe = coe + t.substring(i,i+1);
+                
+            }else if(t.substring(i, i+1).equals("x")||
+            t.substring(i, i+1).equals("*")){
+                return coe;
+            }
+            
+        }
+        return coe;
+    }
+    public static void main(String[] args){
+        System.out.println(getDev("123xx"));
+    }
+    
+}
