54fe046d524e7a4dd141bea33796ee181ce44c65
Hung Che <55414511+Hung-Che@users.noreply.github.com>
2019-09-19 14:55:20 -0500

    Half finish up the derivative part

--- derivative.java
+++ derivative.java
@@ -6,22 +6,45 @@ public class Derivative{
         int l = t.length();
         String coe = "";
         String exp = "";
-        for(int i = 0; i < l; i++)
-            if (!t.substring(i, i + 1).equals("x") &&
+        String ret = "";
+        if(t.indexOf("x")!=0){
+            for(int i = 0; i < l; i++){
+                if (!t.substring(i, i + 1).equals("x") &&
                     !t.substring(i, i + 1).equals("*")) {
-                coe = coe + t.substring(i, i + 1);
-            } else {
-                break;
+                    coe = coe + t.substring(i, i + 1);
+                } else {
+                    break;
+                }
             }
-        for(int j = l; j >= 0; j--){
-            if(!t.substring(j-1,j).equals("^")){
-                exp = t.substring(j-1, j) + exp;
-            }else{
-                break;
+        }else if(t.indexOf("x")==0){
+            coe = "1";
+        }
+        if(t.indexOf("^")==-1&&t.indexOf("x")==-1){
+            exp = "0";
+        }else if(t.indexOf("^")==-1&&t.indexOf("x")!=-1){
+            exp = "1";
+        }else{
+            for(int j = l; j > 0; j--){
+                if(!t.substring(j-1,j).equals("^")){
+                    exp = t.substring(j-1, j) + exp;
+                }else{
+                    break;
+                }
             }
         }
         double a = Double.parseDouble(coe);
         double b = Double.parseDouble(exp);
-        return a*b + " * x ^ " + (b - 1);
+        if(a*b==0){
+            ret = "0";
+        }else if(b-1==0){
+            ret = a*b + "" ;
+        }else if(b-1==1){
+            ret = a*b + " * x ";
+        }else{
+            ret = a*b + " * x ^ " + (b - 1);
+        }
+        return ret;
     }
+
+
 
