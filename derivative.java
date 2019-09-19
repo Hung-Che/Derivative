29da1c6ed4015f349e79c16f7e194f8ab1e620f1
Hung Che <55414511+Hung-Che@users.noreply.github.com>
2019-09-19 15:18:48 -0500

    Split method getDev into 2 methods

--- derivative.java
+++ derivative.java
@@ -3,10 +3,23 @@ public class Derivative{
         t = "";
     }
     public static String getDev(String t){
+        String ret = "";
+        double a = getCoeff(t);
+        double b = getExpon(t);
+        if(a*b==0){
+            ret = "0";
+        }else if(b-1==0){
+            ret = a*b + "" ;
+        }else if(b-1==1){
+            ret = a*b + " * x ";
+        }else{
+            ret = a*b + " * x ^ " + (b - 1);
+        }
+        return ret;
+    }
+    public static double getCoeff(String t){
         int l = t.length();
         String coe = "";
-        String exp = "";
-        String ret = "";
         if(t.indexOf("x")!=0){
             for(int i = 0; i < l; i++){
                 if (!t.substring(i, i + 1).equals("x") &&
@@ -19,6 +32,11 @@ public class Derivative{
         }else if(t.indexOf("x")==0){
             coe = "1";
         }
+        return Double.parseDouble(coe);
+    }    
+    public static double getExpon(String t){
+        int l = t.length();
+        String exp = "";
         if(t.indexOf("^")==-1&&t.indexOf("x")==-1){
             exp = "0";
         }else if(t.indexOf("^")==-1&&t.indexOf("x")!=-1){
@@ -32,19 +50,6 @@ public class Derivative{
                 }
             }
         }
-        double a = Double.parseDouble(coe);
-        double b = Double.parseDouble(exp);
-        if(a*b==0){
-            ret = "0";
-        }else if(b-1==0){
-            ret = a*b + "" ;
-        }else if(b-1==1){
-            ret = a*b + " * x ";
-        }else{
-            ret = a*b + " * x ^ " + (b - 1);
-        }
-        return ret;
+        return Double.parseDouble(exp);
     }
-
-
-
+}
