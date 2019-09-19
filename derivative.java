59481e11586c2ef9ad999be009a4f1f7069383f0
Hung Che <55414511+Hung-Che@users.noreply.github.com>
2019-09-18 20:26:54 -0500

    debugged and added exponent

--- derivative.java
+++ derivative.java
@@ -5,20 +5,23 @@ public class Derivative{
     public static String getDev(String t){
         int l = t.length();
         String coe = "";
+        String exp = "";
         for(int i = 0; i < l; i++){
             if(!t.substring(i, i+1).equals("x")&&
             !t.substring(i, i+1).equals("*")){
                 coe = coe + t.substring(i,i+1);
-                
             }else{
-                return coe;
+                break;
             }
-            
         }
-        return coe;
+        for(int j = l; j >= 0; j--){
+            if(!t.substring(j-1,j).equals("^")){
+                exp = t.substring(j-1, j) + exp;
+            }else{
+                break;
+            }
+        }
+        double a = Double.parseDouble(coe);
+        double b = Double.parseDouble(exp);
+        return a*b + " * x ^ " + (b - 1);
     }
-    public static void main(String[] args){
-        System.out.println(getDev("123xx"));
-    }
-    
-}
