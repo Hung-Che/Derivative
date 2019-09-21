f5ff3929bfe774f141384bbfd15dc14fbeb3ce24
Hung Che <chedaihung4864@gmail.com>
2019-09-20 22:28:59 -0500

    Update derivative.java

--- derivative.java
+++ derivative.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
 public class derivative{
-	public String term(String t){
-
+	public String getTerm(String t, int num){
+		int h = 0;
+		ArrayList<String> terms = new ArrayList<String>();
+		for(int i = 0; i < t.length(); i++){
+			if(t.substring(i, i+1).equals("+")||t.substring(i, i+1).equals("-")){
+				terms.add(t.substring(h, i));
+				h = i;
+			}
+		}
+		if(t.lastIndexOf("+")>t.lastIndexOf("-")&&t.lastIndexOf("+")>0){
+			terms.add(t.substring(t.lastIndexOf("+"), t.length()));
+		}else if(t.lastIndexOf("+")<t.lastIndexOf("-")&&t.lastIndexOf("-")>0){
+			terms.add(t.substring(t.lastIndexOf("-"), t.length()));
+		}
 	}
 }
