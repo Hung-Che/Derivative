539688228f852f04aa652b05d969f311a59bab73
Hung Che <chedaihung4864@gmail.com>
2019-09-21 15:44:05 -0500

    Update derivative.java

--- derivative.java
+++ derivative.java
@@ -1,18 +1,31 @@
 import java.util.ArrayList;
 public class derivative{
 	public String getTerm(String t, int num){
+	    Power p = new Power();
 		int h = 0;
+		String str = "";
 		ArrayList<String> terms = new ArrayList<String>();
 		for(int i = 0; i < t.length(); i++){
 			if(t.substring(i, i+1).equals("+")||t.substring(i, i+1).equals("-")){
-				terms.add(t.substring(h, i));
+				terms.add(p.getDev(t.substring(h, i),num));
 				h = i;
 			}
 		}
 		if(t.lastIndexOf("+")>t.lastIndexOf("-")&&t.lastIndexOf("+")>0){
-			terms.add(t.substring(t.lastIndexOf("+"), t.length()));
+			terms.add(p.getDev(t.substring(t.lastIndexOf("+"), t.length()), num));
 		}else if(t.lastIndexOf("+")<t.lastIndexOf("-")&&t.lastIndexOf("-")>0){
-			terms.add(t.substring(t.lastIndexOf("-"), t.length()));
+			terms.add(p.getDev(t.substring(t.lastIndexOf("-"), t.length()), num));
 		}
+		if(terms.get(0).substring(0,1).equals("+")){
+		    String t1 = terms.get(0).substring(1, terms.get(0).length());
+		    terms.remove(0);
+		    terms.add(0,t1);
+		}
+		for(int i = 0; i < terms.size(); i++){
+		    if(!terms.get(i).equals("0")){
+		        str = str + terms.get(i);
+		    }
+		}
+		return (!str.equals(""))?(str):("0");
 	}
 }
